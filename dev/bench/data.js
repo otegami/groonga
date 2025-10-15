7d790557e45c0cc688bf94d2b29dda5caa15dd82
github-action-benchmark <github@users.noreply.github.com>
2025-10-15 10:03:31 +0900

    add Benchmark (googlecpp) benchmark result for e371e2e7684a368a7ec9a824cf84fe10d46e2e4f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760083414778,
+  "lastUpdate": 1760490211834,
   "repoUrl": "https://github.com/otegami/groonga",
   "entries": {
     "Benchmark": [
@@ -102,6 +102,108 @@ window.BENCHMARK_DATA = {
             "value": 0.01742758299997149,
             "unit": "s/iter",
             "extra": "iterations: 5\ncpu: 0.0015220000000002731 s\nthreads: undefined"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "otegami@clear-code.com",
+            "name": "otegami",
+            "username": "otegami"
+          },
+          "committer": {
+            "email": "otegami@clear-code.com",
+            "name": "otegami",
+            "username": "otegami"
+          },
+          "distinct": true,
+          "id": "e371e2e7684a368a7ec9a824cf84fe10d46e2e4f",
+          "message": "cmake llama.cpp: suppress the `discarded-qualifiers` warning\n\n## Issue\n\nWe got the following error when we built Groonga with MariaDB.\n\n```\n/home/buildbot/_deps/llama_cpp-src/ggml/src/ggml-cpu/ggml-cpu.c:3567:24: error: passing argument 1 of 'putenv' discards 'const' qualifier from pointer target type [-Werro =discarded-qualifiers]\n```\n\n## Cause\n\nWhen building Groonga with MariaDB, the bundled llama.cpp triggers `-Werror`.\n\n## Solution\n\nUpstream llama.cpp doesn't setup discarded-qualifiers,\nso we suppress the warning by explicitly passing `-Wno-discarded-qualifiers` in Debug and RelWithDebInfo builds.",
+          "timestamp": "2025-10-15T09:56:05+09:00",
+          "tree_id": "5d4dfd216ad490043f019ce3a007b45749b75aa3",
+          "url": "https://github.com/otegami/groonga/commit/e371e2e7684a368a7ec9a824cf84fe10d46e2e4f"
+        },
+        "date": 1760490210922,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "stdio: json|json: load/data/multiple",
+            "value": 0.37388028700001996,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.015996999999999734 s\nthreads: undefined"
+          },
+          {
+            "name": "stdio: json|json: load/data/short_text",
+            "value": 0.28494155400011323,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.016413000000000483 s\nthreads: undefined"
+          },
+          {
+            "name": "stdio: json|json: select/olap/multiple",
+            "value": 0.015245476000018243,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.00035899999999977616 s\nthreads: undefined"
+          },
+          {
+            "name": "stdio: json|json: select/olap/n_workers/multiple",
+            "value": 0.021192406000011488,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.0005270000000001107 s\nthreads: undefined"
+          },
+          {
+            "name": "stdio: json|json: wal_recover/db/auto_recovery/column/index",
+            "value": 1.732719650999968,
+            "unit": "s/iter",
+            "extra": "iterations: 1\ncpu: 0.00020799999999976393 s\nthreads: undefined"
+          },
+          {
+            "name": "http: json|json: load/data/multiple",
+            "value": 0.2538199810000492,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.00945100000000007 s\nthreads: undefined"
+          },
+          {
+            "name": "http: json|json: load/data/short_text",
+            "value": 0.14579363600006445,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.008404999999999968 s\nthreads: undefined"
+          },
+          {
+            "name": "http: json|json: select/olap/multiple",
+            "value": 0.017638071000021682,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.0016800000000001813 s\nthreads: undefined"
+          },
+          {
+            "name": "http: json|json: select/olap/n_workers/multiple",
+            "value": 0.0181926870000666,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.0021290000000000475 s\nthreads: undefined"
+          },
+          {
+            "name": "http: apache-arrow|apache-arrow: load/data/multiple",
+            "value": 0.0706937440000388,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.008665999999999827 s\nthreads: undefined"
+          },
+          {
+            "name": "http: apache-arrow|apache-arrow: load/data/short_text",
+            "value": 0.07851365699997359,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.009277999999999731 s\nthreads: undefined"
+          },
+          {
+            "name": "http: apache-arrow|apache-arrow: select/olap/multiple",
+            "value": 0.017613916000073004,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.0016179999999996753 s\nthreads: undefined"
+          },
+          {
+            "name": "http: apache-arrow|apache-arrow: select/olap/n_workers/multiple",
+            "value": 0.008928148999984842,
+            "unit": "s/iter",
+            "extra": "iterations: 5\ncpu: 0.001540000000000319 s\nthreads: undefined"
           }
         ]
       }
